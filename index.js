052fcaddf6b0ebc6a645d66a432487b958af01b2
Nahue <nahue.santini@gmail.com>
2023-11-23 19:14:12 -0300

    changs

--- index.js
+++ index.js
@@ -1,55 +1,73 @@
 const server = require("./src/server");
 const HOST = "192.168.0.46";
 const PORT = 3001;
+const axios = require("axios");
 
 //********************************************************************
 //********************* Servidor + BDD *******************************
 //********************************************************************
-// const { conn } = require("./src/db.js");
-// conn
-//   .sync({ force: true })
-//   .then(() => {
+const { conn } = require("./src/db.js");
+conn
+  .sync({ force: false })
+  .then(() => {
     server.listen(PORT, HOST, () => {
       console.log(`Server listening on ${HOST}:${PORT}`);
     });
-  // })
-  // .catch((error) => console.error(error));
+  })
+  .catch((error) => console.error(error));
 //********************************************************************
 //********************************************************************
 
 //????????????????????????????????????????????????????????????????????????
 //?????????????????????? Conexion con EMQX ???????????????????????????????
 //????????????????????????????????????????????????????????????????????????
-// const mqtt = require("mqtt");
-// const TOPIC = "64c314be56857449102a9d4b/testid/NrFMgh03GO/sdata";
+const mqtt = require("mqtt");
+// const TOPIC = "64c314be56857449102a9d4b/testid/aPtCeiVxcp/sdata"; //din0
+// const TOPIC = "64c314be56857449102a9d4b/testid/O2RMRlSUYU/sdata" //din1;
+// const TOPIC = "64c314be56857449102a9d4b/testid/DpG13PisLO/sdata" //din2;
+// const TOPIC = "64c314be56857449102a9d4b/testid/9UaEQR4I36/sdata" //din3;
+// const TOPIC = "64c314be56857449102a9d4b/testid/NrFMgh03GO/sdata" //ain0;
+// const TOPIC = "64c314be56857449102a9d4b/testid/iSK4MVs6tO/sdata" //ain1;
+// const TOPIC = "64c314be56857449102a9d4b/testid/AM8p6BX0gm/sdata" //ain2;
+// const TOPIC = "64c314be56857449102a9d4b/testid/rgzxwhfl36/sdata" //ain3;
+//////////////////////////////////////////////////////////////////////////////
+// const TOPIC = "64c314be56857449102a9d4b/testid/nLngKM48P/sdata" //out0;
+// const TOPIC = "64c314be56857449102a9d4b/testid/uX0NxQhNrr/sdata" //out1;
+// const TOPIC = "64c314be56857449102a9d4b/testid/7AJDDkZVrj/sdata" //out2;
+// const TOPIC = "64c314be56857449102a9d4b/testid/1QMLeg5CT4/sdata" //out3;
+//////////////////////////////////////////////////////////////////////////////
+const TOPIC = "64c314be56857449102a9d4b/testid/inLngKM48P/actdata" //ACTout0;
+// const TOPIC = "64c314be56857449102a9d4b/testid/uX0NxQhNrr/actdata" //ACTout1;
+// const TOPIC = "64c314be56857449102a9d4b/testid/7AJDDkZVrj/actdata" //ACTout2;
+// const TOPIC = "64c314be56857449102a9d4b/testid/1QMLeg5CT4/actdata" //ACTout3;
 
-// const client = mqtt.connect(`mqtt://${HOST}/mqtt`);
-// client.on("connect", () => {
-//   console.log("Conectado al broker MQTT");
+const client = mqtt.connect(`mqtt://${HOST}/mqtt`);
+client.on("connect", () => {
+  console.log("Conectado al broker MQTT");
 
-//   // Suscribirse a los topicos
-//   client.subscribe(TOPIC, (error) => {
-//     if (error) {
-//       console.error(`Error al suscribirse a ${TOPIC}:`, error);
-//     } else {
-//       console.log(`Suscrito a ${TOPIC}`);
-//     }
-//   });
-// });
+  // Suscribirse a los topicos
+  client.subscribe(TOPIC, (error) => {
+    if (error) {
+      console.error(`Error al suscribirse a ${TOPIC}:`, error);
+    } else {
+      console.log(`Suscrito a ${TOPIC}`);
+    }
+  });
+});
 
-// // Recibir mensajes
-// client.on("message", (topic, message) => {
-//   console.log(`Mensaje recibido en el tema ${topic}: ${message.toString()}`);
-// });
+// Recibir mensajes
+client.on("message", (topic, message) => {
+  console.log(`Mensaje recibido en el tema ${topic}: ${message.toString()}`);
+});
 
-// // Publicar un mensaje después de conectarse
-// const mensaje = "Hola, este es un mensaje de prueba";
-// client.publish(TOPIC, mensaje, (error) => {
-//   if (error) {
-//     console.error(`Error al publicar en ${TOPIC}:`, error);
-//   } else {
-//     console.log(`Mensaje publicado en ${TOPIC}: ${mensaje}`);
-//   }
-// });
+// Publicar un mensaje después de conectarse
+const mensaje = "{'value': 'true'}";
+client.publish(TOPIC, mensaje, (error) => {
+  if (error) {
+    console.error(`Error al publicar en ${TOPIC}:`, error);
+  } else {
+    console.log(`Mensaje publicado en ${TOPIC}: ${mensaje}`);
+  }
+});
 //????????????????????????????????????????????????????????????????????????
 //????????????????????????????????????????????????????????????????????????
--- src/middleware/post.js
+++ src/middleware/post.js
@@ -1,93 +1,15 @@
+const postUser = require("../controllers/postUser");
+
 const post = async (req, res) => {
-    const { dId, password, variables } = req.body;
-    console.log(req.body);
-  
-    try {
-      //const usuario = await postUser({ dId, password, variables });
-  
-      res.status(200).json({ 
-        username: "5mhCCz1g9Y", 
-        password: "uj4jRrd3Zr", 
-        clientId: "device_testid_89681", 
-        topic: "64c314be56857449102a9d4b/testid/", 
-        variables: [ 
-            { 
-                "variableFullName": "din0", 
-                "variableType": "input", 
-                "variable": "aPtCeiVxcp", 
-                "variableSendFreq": "30" 
-            }, 
-            { 
-                "variableFullName": "din1", 
-                "variableType": "input", 
-                "variable": "O2RMRlSUYU", 
-                "variableSendFreq": "30" 
-            }, 
-            { 
-                "variableFullName": "din2", 
-                "variableType": "input", 
-                "variable": "DpG13PisLO", 
-                "variableSendFreq": 120 
-            }, 
-            { 
-                "variableFullName": "din3", 
-                "variableType": "input", 
-                "variable": "9UaEQR4I36", 
-                "variableSendFreq": 120 
-            }, 
-            { 
-                "variableFullName": "ain0", 
-                "variableType": "input", 
-                "variable": "NrFMgh03GO", 
-                "variableSendFreq": "10" 
-            }, 
-            { 
-                "variableFullName": "ain1", 
-                "variableType": "input", 
-                "variable": "iSK4MVs6tO", 
-                "variableSendFreq": "10" 
-            }, 
-            { 
-                "variableFullName": "ain2", 
-                "variableType": "input", 
-                "variable": "AM8p6BX0gm", 
-                "variableSendFreq": 120 
-            }, 
-            { 
-                "variableFullName": "ain3", 
-                "variableType": "input", 
-                "variable": "rgzxwhfl36", 
-                "variableSendFreq": 120 
-            }, 
-            { 
-                "variableFullName": "out0", 
-                "variableType": "output", 
-                "variable": "inLngKM48P", 
-                "variableSendFreq": 120 
-            }, 
-            { 
-                "variableFullName": "out1", 
-                "variableType": "output", 
-                "variable": "uX0NxQhNrr", 
-                "variableSendFreq": 120 
-            }, 
-            { 
-                "variableFullName": "out2", 
-                "variableType": "output", 
-                "variable": "7AJDDkZVrj", 
-                "variableSendFreq": 120 
-            }, 
-            { 
-                "variableFullName": "out3", 
-                "variableType": "output", 
-                "variable": "1QMLeg5CT4", 
-                "variableSendFreq": 120 
-            } 
-        ] 
-    });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  };
-  
-  module.exports = post;
+  const { dId, password, variables } = req.body;
+
+  try {
+    const usuario = await postUser({ dId, password, variables });
+
+    res.status(200).json(usuario);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = post;
