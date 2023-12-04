40e259ab8c0acc4925297b0d65e6a8d5dba61886
Nahue <nahue.santini@gmail.com>
2023-12-04 16:47:02 -0300

    script

---
+++ 3001.bat
@@ -0,0 +1,10 @@
+@Echo off
+
+:home
+cls
+
+c:
+cd\
+cd Users\santi\Escritorio\nahuelnob\PROGRAMACION\Endast\EndastServer
+@npm start
+goto End
---
+++ 3001.vbs
@@ -0,0 +1,3 @@
+Set WshShell = CreateObject("WScript.Shell")
+WshShell.Run chr(34) & "3001.bat" & Chr(34), 0
+Set WshShell = Nothing 
